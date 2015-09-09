da94a92d552a213e6d6c65b8a6e0d2349078384c
Sarah Edkins <edkins.sarah@gmail.com>
2015-09-08 21:29:20 -0400

    Complete

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
 // Use Node.js to perform profile look-ups and serve our template via HTTP.
-
-// 1. Create a web server
-
+var router = require("./route.js");
+// Create a web server
 var http = require('http');
 
 http.createServer(function (request, response) {
-    resquest.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+    router.home(request, response);
+    router.user(request, response);
 }).listen(3000, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:1337/');ß
+console.log('Server running at http://127.0.0.1:3000/');
 
-// 2. Handle HTTP route GET / and POST / i.e. Home
-    // if URL == '/' && GET
-        // show search
-    // if URL == '/' && POST
-        // redirect to /:username
-
-// 3. Handle HTTP route GET /:username i.e. /chalkers
-    // if URL == '/....'
-        // get JSON from Treehouse
-    // on 'end'
-        // show profile
-    // on 'error'
-        // show error
-
-// 4. Function that handles the reading of files and merge in values
-    // read from file and get a string
-    // merge values into string
 
---
+++ renderer.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+
+function mergeValues(values, content) {
+    // Cycle over the keys
+    for (var key in values) {
+        // Replace all {{key}} with the value from the values object
+        content = content.replace("{{" + key + "}}", values[key]);
+    }
+    // return merged content
+    return content;
+}
+
+function view(templateName, values, response) {
+
+    // read from the template file
+    var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
+
+    // insert values in to the content
+    fileContents = mergeValues(values, fileContents);
+
+    // write out to the response
+    response.write(fileContents);
+
+}
+
+module.exports.view = view;
